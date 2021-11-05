a6c257a599ec03d6f151e1b609d821b092788794
MarieJoker <63437058+MarieJoker@users.noreply.github.com>
2021-11-05 13:25:18 -0600

    Delete loadstring.lua

--- loadstring.lua
+++
@@ -1,7 +0,0 @@
---[[
-    Games: Prison Life
-           Strucid
-           Trade Tower
-           Entry Point
-]]--
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MarieJoker/unknown/main/main.lua"))()
