a72bd4952c5051b1ae98e55ce6624c282f7d85a2
MarieJoker <63437058+MarieJoker@users.noreply.github.com>
2022-04-23 13:33:51 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,21 +5,11 @@
 
 local game = game;
 
-if not game:IsLoaded() then
-    repeat
-        task.wait();
-    until game:IsLoaded();
-end;
+game.Loaded:Wait();
 
 local hook_meta_method = hookmetamethod;
 
 local get_namecall_method = getnamecallmethod or get_namecall_method;
-
-local hookfunc = hookfunction or hookfunc;
-
-if hookfunc then
-    hookfunc(error, warn);
-end;
 
 if hook_meta_method then
 do
@@ -34,22 +24,10 @@ end;
 
 local games = {
     [2625195454] = "ep",
-    [3200010305] = "ep",
-    [2797881676] = "ep",
-    [2951213182] = "ep",
-    [3590667014] = "ep",
-    [4518266946] = "ep",
-    [4661507759] = "ep",
-    [4768829954] = "ep";
 };
 
 local success, result = pcall(function()
-    return game:HttpGetAsync(string.format("https://github.com/MarieJoker/unknown/blob/main/games/%s.lua?raw=true", games[game.PlaceId]));
+    return game:HttpGet(string.format("https://github.com/MarieJoker/unknown/blob/main/games/%s.lua?raw=true", games[game.PlaceId] or "universal"));
 end);
 
-if success then
-        loadstring(result)();
-    else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/MarieJoker/unknown/main/games/universal.lua", true))();
-    end;
-end;
+if success then loadstring(result)() end;
